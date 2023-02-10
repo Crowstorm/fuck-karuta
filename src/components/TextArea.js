dd3986de1e1a2b24abeb751d15b5708da920294c
Crowstorm <crowstormofficial@gmail.com>
2023-02-10 11:20:12 +0100

    add placeholder

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -19,6 +19,16 @@ const TextArea = ({ setCodes }) => {
             style={{ width: '50%' }}
             rows={30}
             onChange={(e) => onChange(e.target.value)}
+            placeholder="🔥 c1h5b2 · ☆☆☆☆ · #11976 · ◈3 · Vlad Love · Mai Vlad Transylvania
+            ◾ c1q2vn · ☆☆☆☆ · #47914 · ◈1 · One-Punch Man · Mosquito Girl
+            ⚛️ c1vsnz · ★★☆☆ · #5754  · ◈2 · Genshin Impact · Azhdaha
+            ◾ c1q39k · ★★☆☆ · #23940 · ◈2 · The Ancient Magus' Bride · Redcurrant
+            ◾ cntlgh · ★★☆☆ · #5450  · ◈2 · Fire Emblem: Radiant Dawn · Heather
+            💰 cnpbq4 · ★☆☆☆ · #248   · ◈5 · Princess Mononoke · San
+            💹 cnr153 · ★☆☆☆ · #16168 · ◈1 · Mother of the Goddess' Dormitory · Fuki Mamoru-kun
+            💹 cnz81v · ★★☆☆ · #20020 · ◈2 · Solo Leveling · Yoo Soo-Hyun
+            ◾ cnz1qq · ★★☆☆ · #414   · ◈4 · Blue Period · Haruka Hashida
+            💮 cnz33l · ★★☆☆ · #48552 · ◈1 · Anohana: The Flower We Saw That Day · Teacher"
         />
     )
 }
